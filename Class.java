9b60e06517e8e46aa3ba3372fd03dba4cff728ce
Aro Barath Chandru <130319315+cyber-bytezz@users.noreply.github.com>
2025-04-25 10:50:57 +0530

    Update Class.java

--- Class.java
+++ Class.java
@@ -4,17 +4,15 @@ import java.util.*;
 // Read only region start
 class UserMainCode
 {
-    public String asciiSumCheck(String[] input1, int input2, int input3){
+    public int maxOddAdjacentPair(int input1, int[] input2){
         // Read only region end
-        for (int i = 0; i < input2; i++) {
-            char a = input1[i].charAt(0);
-            for (int j = i + 1; j < input2; j++) {
-                char b = input1[j].charAt(0);
-                if ((int)a + (int)b == input3) {
-                    return "" + a + b;
-                }
+        int maxSum = -1;
+        for (int i = 0; i < input1 - 1; i++) {
+            int sum = input2[i] + input2[i + 1];
+            if (sum % 2 != 0 && sum > maxSum) {
+                maxSum = sum;
             }
         }
-        return "-1";
+        return maxSum;
     }
 }
